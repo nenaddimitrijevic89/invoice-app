57ba7586e320fd8de668b5b5d3a47cb11863591f
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2022-11-03 00:12:22 +0100

    Add invoice model

---
+++ src/models/Invoice.js
@@ -0,0 +1,41 @@
+class Invoice {
+   constructor({
+      id,
+      createdAt,
+      paymentDue,
+      description,
+      paymentTerms,
+      clientName,
+      clientEmail,
+      status,
+      senderAddress,
+      clientAddress,
+      items,
+      total,
+   }) {
+      this.id = id
+      this.createdAt = createdAt
+      this.paymentDue = paymentDue
+      this.description = description
+      this.paymentTerms = paymentTerms
+      this.clientName = clientName
+      this.clientEmail = clientEmail
+      this.status = status
+      this.senderAddress = {
+         street: senderAddress.street,
+         city: senderAddress.city,
+         postCode: senderAddress.postCode,
+         country: senderAddress.country,
+      }
+      this.clientAddress = {
+         street: clientAddress.street,
+         city: clientAddress.city,
+         postCode: clientAddress.postCode,
+         country: clientAddress.country,
+      }
+      this.items = items
+      this.total = total
+   }
+}
+
+export default Invoice
