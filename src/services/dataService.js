6955701dc5c47ec7ed7cb996c089f77b3d1547aa
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2022-11-07 00:08:16 +0100

    Implement invoice service in provider

--- src/services/dataService.js
+++ src/services/dataService.js
@@ -1,26 +1,13 @@
 import Invoice from 'models/Invoice'
 
-export const fetchData = async setData => {
-   try {
-      const response = await fetch('data.json', {
-         headers: {
-            'Content-Type': 'application/json',
-            Accept: 'application/json',
-         },
-      })
-      const data = await response.json()
-      const mapped = data.map(invoice => new Invoice(invoice))
-
-      setData(mapped)
-   } catch (err) {
-      console.error(err)
-   }
-}
-
 class InvoiceService {
+   url = {
+      invoices: 'data.json',
+   }
+
    async fetchInvoices(callback) {
       try {
-         const response = await fetch('data.json', {
+         const response = await fetch(this.url.invoices, {
             headers: {
                'Content-Type': 'application/json',
                Accept: 'application/json',
