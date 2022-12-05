c85904f7307df8894d38642d254efa1b79416725
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2022-12-05 11:47:20 +0100

    WIP: Update string file

--- src/res/strings.js
+++ src/res/strings.js
@@ -2,9 +2,11 @@ export const strings = {
    rs: {
       invoices: 'Invoices',
       newInvoice: 'New Invoice',
+      billFrom: 'Bill From',
    },
    en: {
       invoices: 'Fakture',
       newInvoice: 'Nova Faktura',
+      billFrom: 'Račun od',
    },
 }
