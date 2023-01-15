0889b474ee6994a17b6aa2fe747b27511228da84
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2023-01-15 18:13:41 +0100

    Translate field item labels

--- src/components/FieldItemLabels.jsx
+++ src/components/FieldItemLabels.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 import { Grid, GridItem, Text } from '@chakra-ui/react'
 
 const FieldItemLabels = () => {
+   const { t } = useTranslation()
    return (
       <Grid templateColumns="repeat(12, 1fr)" gap={2} w="100%">
          <GridItem colSpan={4}>
-            <Text textStyle="body1">Item Name</Text>
+            <Text textStyle="body1">{t('itemName')}</Text>
          </GridItem>
          <GridItem colSpan={1}>
-            <Text textStyle="body1">Qty</Text>
+            <Text textStyle="body1">{t('qty')}</Text>
          </GridItem>
          <GridItem colSpan={3}>
-            <Text textStyle="body1">Price</Text>
+            <Text textStyle="body1">{t('price')}</Text>
          </GridItem>
          <GridItem colSpan={3}>
-            <Text textStyle="body1">Total</Text>
+            <Text textStyle="body1">{t('total')}</Text>
          </GridItem>
       </Grid>
    )
